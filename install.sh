bfae7d99c1feee419b552c2ad14672f8b66183f0
Matt Burke <burkemd1+github@gmail.com>
2014-09-30 20:35:11 -0400

    add an install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Installs all scripts to the ~/bin directory as soft links
+SCRIPT_NAME=$(basename $0)
+
+function simlink() {
+	if [ -f "$HOME/bin/$1" ]; then
+		echo "Skippng $1"
+	else
+		echo "Linking $1"
+		ln -s "$(realpath $1)" "$HOME/bin/$1"
+	fi
+}
+
+for f in *; do
+	if [ "$f" != "$SCRIPT_NAME" ]; then
+		simlink $f
+	fi
+done
